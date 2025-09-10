35ff12b675b5e11bb56618f61f67d61864c8fb1f
LovarajuDivili <ldivili@its.jnj.com>
2025-09-10 14:11:14 +0530

    feat: added common folder with files

---
+++ src/common/labelConstants.tsx
@@ -0,0 +1,17 @@
+
+export const HomePage = {
+    AIFA : 'AIFA',
+}
+
+export const HomePage_Dropdowns = {
+    ADMIN: 'Admin',
+    TECHNICAL : 'Technical',
+    TRAINING : 'Training',
+    HR : 'HR',
+}
+
+
+// If you want to use this variables in your component
+// Example:
+// import { HomePage } from 'src/common/labelConstants';
+// <Typography>{HomePage.AIFA}}</Typography>
