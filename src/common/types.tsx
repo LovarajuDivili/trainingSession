f34ee1e689f7c2727ad419edc93414ac3581bbf4
SukanyaYerrabati1 <Sukanya.Y@aifalabs.com>
2025-09-15 15:36:46 +0530

    fix: updated ypes.tsx

--- src/common/types.tsx
+++ src/common/types.tsx
@@ -1,5 +1,12 @@
+import type { JSX } from "react";
 
 export interface DropdownItem {
     label: string;
     value: string;
+}
+
+export interface DropdownItem {
+  value: string;
+  label: string;
+  icon: JSX.Element;
 }
