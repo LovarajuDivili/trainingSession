64c5bc65045ce28566737b878867095283a8e0df
SukanyaYerrabati1 <Sukanya.Y@aifalabs.com>
2025-09-15 18:32:43 +0530

    feat: update dashboard page for All Employees

--- src/common/types.tsx
+++ src/common/types.tsx
@@ -1,12 +1,25 @@
 import type { JSX } from "react";
 
-export interface DropdownItem {
+/*export interface DropdownItem {
     label: string;
     value: string;
-}
+}*/
 
 export interface DropdownItem {
   value: string;
   label: string;
   icon: JSX.Element;
+}
+
+export interface Employee {
+  name: string;
+  email: string;
+  role: string;
+  joinDate: string;
+  id: string;
+  skills: string[];
+}
+
+export interface DashboardProps {
+  selectedItem: string;
 }
--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,15 +1,27 @@
-// AdminPage.tsx
 import { Box } from "@mui/material";
 import Header from "../components/Header";
+import { useState } from "react";
 import SideBar from "../components/SideBar";
+import Dashboard from "../components/Dashboard";
 
-const AdminPage = () => {
+const Admin = () => {
+  const [selectedItem, setSelectedItem] = useState("All Employees");
+
   return (
-    <Box mt={5}>
-      <Header role={""} />
-      <SideBar />
+    <Box>
+      
+      <Box sx={{ position: "fixed", top: 0, left: 0, right: 0, zIndex: 1000 }}>
+        <Header role={""} />
+      </Box>
+
+      <Box sx={{ display: "flex", mt: "50px" }}> 
+        <SideBar selectedItem={selectedItem} setSelectedItem={setSelectedItem} />
+        <Box sx={{ flexGrow: 1, ml: "240px", p: 3 }}> 
+          <Dashboard selectedItem={selectedItem} />
+        </Box>
+      </Box>
     </Box>
   );
 };
 
-export default AdminPage;
+export default Admin;
