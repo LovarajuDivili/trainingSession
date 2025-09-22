3a708b2d9c416211567029fcf35a30acfccff341
LovarajuDivili <ldivili@its.jnj.com>
2025-09-22 18:09:34 +0530

    feat: python setup configured

---
+++ backend/app/routeLayout.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+from app.routes.reference.users import router as users_router
+from app.routes.reference.projects import router as projects_router
+
+api_router = APIRouter()
+api_router.include_router(projects_router)
+api_router.include_router(users_router)
