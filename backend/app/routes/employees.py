727c1eebe05110384077bd6b1b50d34ba3b9f380
SukanyaYerrabati1 <Sukanya.Y@aifalabs.com>
2025-10-08 11:50:55 +0530

    fix: route layout

--- backend/app/routes/employees.py
+++ backend/app/routes/employees.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, HTTPException
 from ..database import db
 from bson import ObjectId
 from datetime import datetime
